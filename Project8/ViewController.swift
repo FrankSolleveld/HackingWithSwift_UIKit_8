29b687efb3742b593fe93858c00c35ed0595a2b0
FrankSolleveld <fsolleveld@outlook.com>
2021-03-01 12:52:54 +0100

    added base code and constraint to score label

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -9,10 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Custom Variables and IBOutlets
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    // MARK: - Lifecycle Methods
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    // MARK: - Custom Methods
+    
+    
+    // MARK: - Delegate Methods
 
 
 }
